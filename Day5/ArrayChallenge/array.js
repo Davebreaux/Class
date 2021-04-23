7f945a6f584a129fccef7bc4ac5df33e7973a704
David Breaux <74641618+Davebreaux@users.noreply.github.com>
2021-04-23 10:38:37 -0500

    as

--- Day5/ArrayChallenge/array.js
+++ Day5/ArrayChallenge/array.js
@@ -58,11 +58,11 @@ console.log(result3); // we expect back 4
 function fibonacciArray(n) {
     // the [0, 1] are the starting values of the array to calculate the rest from
     var fibArr = [0, 1];
-    for (var i = 0; i <= 10; i++) {
-        fibArr.push(arr[i] + arr[i+1])
+    for (var i = 0; i <= n; i++) {
+        fibArr.push((fibArr[i] + fibArr[i+1]))
     }
     return fibArr;
 }
     
 var result4 = fibonacciArray(10);
-console.log(result4); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+console.log(result4);// we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
