d57d5bf9f7c73340177b8f1c8965598593a8fe2f
David Breaux <74641618+Davebreaux@users.noreply.github.com>
2021-04-29 13:06:31 -0500

    asdf

--- Week2/Day4/Calculator/script.js
+++ Week2/Day4/Calculator/script.js
@@ -1,2 +1,54 @@
 var displayDiv = document.querySelector("#display");
-displayDiv.innerText = "Some new value";
+
+displayDiv.innerText = `0`;
+
+var inputNum = "";
+
+var operator = "";
+
+var number1 = 0
+
+var number2 = 0
+
+var answer = ""
+
+function press(n) {
+    inputNum += n;
+    console.log(inputNum)
+    displayDiv.innerText = `${inputNum}`;
+}
+
+function setOP(op) {
+    number1 = parseFloat(inputNum);
+    operator = op;
+    inputNum = ""
+}
+
+function calculate() {
+    number2 = parseFloat(inputNum);
+    switch (operator) {
+        case "+":
+            answer = `${number1 + number2}`
+            break;
+        case "-":
+            answer = `${number1 - number2}`
+            break;
+        case "*":
+            answer = `${number1 * number2}`
+            break;
+        case "/":
+            answer = `${number1 / number2}`
+            break;
+        default:
+            break;
+    }
+    displayDiv.innerText = answer;
+}
+
+function clr() {
+    displayDiv.innerText = `0`;
+    inputNum = ``
+    operator = ``
+    number1 = 0
+    number2 = 0
+}
